8804e7ead550aff2fb1b3bea45a2e7e52592c211
Nitin-Raster <nitink.kolapkar@gmail.com>
2024-11-23 14:14:57 +0530

    For, While, Do-While Loop assignment added

---
+++ Loops/Do_While_Loop_Decremental_Count.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	// Variable Declarations
+	int i_nkk = 10;
+
+	// Code
+	do
+	{
+		printf("The Value of i_nkk is %d\n", i_nkk);
+		i_nkk--;
+	} while (i_nkk >= 1);
+
+	return(0);
+}
---
+++ Loops/While_Loop_Incremental_Count.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	// Variable Declarations
+	int i_nkk = 1;
+
+	// Code
+	while (i_nkk <= 10)
+	{
+		printf("The Value of i_nkk is %d\n", i_nkk);
+		i_nkk++;
+	}
+
+	return(0);
+}
